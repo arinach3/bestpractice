040229713659ab6492b2706fa0a2b58bfb33cff5
Ariella Vaystooch <arinach3@gmail.com>
2025-07-20 22:18:13 +0300

    first react iteration

---
+++ src/DependencyManagement/lesson1.jsx
@@ -0,0 +1,16 @@
+import bigInt from "big-integer";
+import { swapCase } from "swap-case";
+
+console.log(bigInt(5).toString(10, 'ABCDEFGHIJKLMNOP'));
+console.log(swapCase("hELLO wORLD"));
+console.log(swapCase("Hello World"));
+
+export default function Lesson1(){
+    return(
+        <div>
+            <p>Best practice</p>
+            
+
+        </div>
+    )
+}
--- src/DependencyManagement/main.js
+++
@@ -1,3 +0,0 @@
-import bigInt from "big-integer";
-
-console.log(bigInt(5).toString(10, 'ABCDEFGHIJKLMNOP'));
