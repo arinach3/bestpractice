6ee33037b50e70d3c289929af90d7891f9147255
Ariella Vaystooch <arinach3@gmail.com>
2025-07-20 22:24:23 +0300

    first recipe book commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
 import Lesson1 from "./DependencyManagement/lesson1"
+import RecipeBook from './RecipeBook/RecipeBook';
 
 function App() {
   return (
     <div className="App">
       
       <Lesson1 />
+      <RecipeBook />
     </div>
 
 
---
+++ src/RecipeBook/RecipeBook.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+export default function RecipeBook(){
+    return(
+        <div>
+            <h1>Choose a recipe</h1>
+        </div>
+    )
+}
