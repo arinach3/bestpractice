8ddb1e288ca3908aa855995a819bd5b8d87e1892
Ariella Vaystooch <arinach3@gmail.com>
2025-07-27 21:41:29 +0300

    added favirotie button

--- src/RecipeBook/RecipeBook.jsx
+++ src/RecipeBook/RecipeBook.jsx
@@ -1,10 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import SideBar from './SideBar';
 import "./RecipeBook.css";
 
 export default function RecipeBook(){
+    const [favorite, setFavorite] = useState([]);
 
+    useEffect(() => {const stored = JSON.parse(localStorage.getItem('favorite')) || [];
+    setFavorite(stored);}, []);
+
+    useEffect(() => {
+        localStorage.setItem('favorite', JSON.stringify(favorite)); 
+    }, [favorite]);
+
+    const toggleFavorite = (id) => {
+        if(favorite.includes(id)) {
+            setFavorite(favorite.filter(favId => favId !== id));
+        }else {
+            setFavorite([...favorite, id]);
+        }
+    };
+
+    const isFavorite = (id) => favorite.includes(id);
+    
 
     return(
         <div class="book">
--- src/RecipeBook/RecipePage.jsx
+++ src/RecipeBook/RecipePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import recipes from './Recipes';
 import SideBar from './SideBar';
@@ -9,7 +9,24 @@ import "./RecipeBook.css";
 export default function RecipePage(){
     const { id } = useParams();
     const recipe = recipes.find((r) => r.id === id);
-    console.log(id);
+    const [favorite, setFavorite] = useState(() => {
+    const stored = localStorage.getItem('favorites');
+    return stored ? JSON.parse(stored) : [];
+  });
+
+    useEffect(() => {
+        localStorage.setItem('favorites', JSON.stringify(favorite)); 
+    }, [favorite]);
+
+    const toggleFavorite = (id) => {
+        if(favorite.includes(id)) {
+            setFavorite(favorite.filter(favId => favId !== id));
+        }else {
+            setFavorite([...favorite, id]);
+        }
+    };
+
+    const isFavorite = (id) => favorite.includes(id);
 
     if (!recipe) return <p>Recipe not found</p>;
 
@@ -27,6 +44,7 @@ export default function RecipePage(){
                 <ol>
                     {recipe.instructions.map((item, i) => <li class="recipe" key={i}>{item}</li>)}
                 </ol>
+                <button class="favButton" onClick={() => toggleFavorite(recipe.id)}>{isFavorite(recipe.id) ? "💔Remove from favorites" : "💜 Add to favorites"}</button>
             </div>
         </div>
     );
