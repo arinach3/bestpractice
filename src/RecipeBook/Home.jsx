72ae772fd1d2066e3732f60d995bc3f6aeb1a87c
Ariella Vaystooch <arinach3@gmail.com>
2025-08-07 21:51:33 +0300

    adding unit testing for seach feature

--- src/RecipeBook/Home.jsx
+++ src/RecipeBook/Home.jsx
@@ -7,12 +7,12 @@ export default function Home(){
     const [searchTerm, setSearchTerm] = useState("");
     const filteredRecipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
-    console.log(filteredRecipes);
+    console.info("filtering recipes")
     return(
         <div>
             <h4>Search for a recipe here</h4>
             <input type="text" placeholder='enter recipe name' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
-            {filteredRecipes.length === 0 ? (
+            {filteredRecipes.length === 0 ? (         
             <p>No recipe found</p>
             ):(
         
