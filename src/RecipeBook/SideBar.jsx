c5a19d4a690af67e59459efd3ce5fa6eb8ccbda8
Ariella Vaystooch <arinach3@gmail.com>
2025-08-10 14:41:23 +0300

    added tests to search feature

--- src/RecipeBook/SideBar.jsx
+++ src/RecipeBook/SideBar.jsx
@@ -12,10 +12,10 @@ export default function SideBar({ active }){
     return(
         <div className="sidebar">
       <h2>Choose a recipe</h2>
-      <Link to="/favorites">Favorites</Link>
+      <Link to="/favorites" onClick={() => console.info("favorite menu clicked")}>Favorites</Link>
 
       {recipes.map((recipe) => (
-            <Link to={`/recipes/${recipe.id}`} key={recipe.id}>
+            <Link to={`/recipes/${recipe.id}`} key={recipe.id} onClick={() => console.info("Clicked recipe: " + recipe.title)}>
           <li class={`${recipe.id}` === id ? "selected" : ""}>{recipe.title}</li>
         </Link>))}
         
